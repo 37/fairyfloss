dce291697748038ddb06dca131101b1f66acb2bc
Psychat <keaton.okkonen@gmail.com>
2015-04-22 11:44:58 +1000

    commit

--- app.js
+++ app.js
@@ -14,10 +14,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.set('port', process.env.PORT || 3000);
-var server = app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + server.address().port);
-});
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
