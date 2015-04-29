8f2605c89331ee72e4793dfc3fa4706ba0cec00e
Psychat <keaton.okkonen@gmail.com>
2015-04-29 22:24:02 +1000

    making it better

--- public/javascripts/form-functions.js
+++ public/javascripts/form-functions.js
@@ -1,4 +1,56 @@
+// ======================================================================================
+// 			Global variables
+// ======================================================================================
+
 var addincount = 0;
+
+var tableref = [
+	['box0', 'box1', 'box2', 'box3', 'box4', 'box5'],
+	['box6', 'box7', 'box8', 'box9', 'box10', 'box11'],
+	['box12', 'box13', 'box14', 'box15', 'box16', 'box17'],
+	['box18', 'box19', 'box20', 'box21', 'box22', 'box23'],
+	['box24', 'box25', 'box26', 'box27', 'box28', 'box29'],
+	['box30', 'box31', 'box32', 'box33', 'box34', 'box35']
+];
+
+// ======================================================================================
+// 			Misc styling functions
+// ======================================================================================
+
+function textAreaAdjust(o) {
+	o.style.height = "1px";
+	o.style.height = (25+o.scrollHeight)+"px";
+}
+
+function gridHoverStyle(grid) {
+
+	resetGrid();
+
+	for(y = 0; y < 6; y++){
+		for(x = 0; x < 6; x++){
+			if (((y * 6) + x) == grid){
+				var ty = y + 1;
+				var tx = x + 1;
+			}
+		}
+	}
+
+	for(i = 0; i < ty; i++) {
+		for(j = 0; j < tx; j++){
+			var boxid = tableref[i][j];
+			document.getElementById(boxid).className="gridbox grid-hover";
+		}
+	}
+}
+
+function resetGrid() {
+	for(i = 0; i < 6; i++) {
+		for(j = 0; j < 6; j++){
+			var boxid = tableref[i][j];
+			document.getElementById(boxid).className="gridbox";
+		}
+	}
+}
 
 
 // ======================================================================================
@@ -163,7 +215,7 @@ function add_radio(elementName){
 						</button>';
 
 	document.getElementById(elementName).appendChild(newdiv);
-	
+
 	linecount++;
 }
 
@@ -176,6 +228,33 @@ function add_radio(elementName){
 // 			Static element functions || **END INPUT GENERATOR FUNCTIONS
 // ======================================================================================
 
+function add_static(elementName, buildtype, displaytype, class_modifier, icon, settings_action){
+
+	var newdiv = document.createElement('div');
+	newdiv.className = "element-container";
+		var element_html = '<textarea placeholder="Enter your text content here." onkeyup="textAreaAdjust(this)" required="required" name="data[' + addincount + '][0]" class="input ' + class_modifier + '"></textarea>';
+	if (buildtype == 'textarea') {
+
+	} else if (buildtype == 'text') {
+		var element_html = '<input type="' + buildtype + '" placeholder="Enter your default text here." required="required" name="data[' + addincount + '][0]" class="input ' + class_modifier + '" />';
+	} else if (buildtype == 'table') {
+		var element_html = '';
+	}
+
+	newdiv.innerHTML = '' + element_html + '\
+						<input type="hidden" required="required" name="data[' + addincount + '][1]" value="' + displaytype + '" /> \
+						<input type="hidden" required="required" name="data[' + addincount + '][2]" value="' + elementName + '" /> \
+						<div class="form-button option-icon"> \
+							<i class="fa fa-' + icon + '"></i> \
+						</div> \
+						<button id="element-settings' + addincount + '" type="button" onclick="' + settings_action + '" class="form-button settings-button"> \
+							<i class="fa fa-cog"></i> \
+						</button>';
+
+	document.getElementById(elementName).appendChild(newdiv);
+
+	addincount++;
+}
 
 
 // ======================================================================================
