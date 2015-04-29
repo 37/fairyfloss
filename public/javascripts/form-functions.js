ae8281860afbc950d2fff367b3d96cd850c03730
Psychat <keaton.okkonen@gmail.com>
2015-04-29 15:17:09 +1000

    commit

--- public/javascripts/form-functions.js
+++ public/javascripts/form-functions.js
@@ -18,29 +18,32 @@ function add_textarea(elementName){
 
 function add_checkbox_base(elementName){
 	var newdiv = document.createElement('div');
+	var checkquot = "'checkbox" + addincount + "'";
 	newdiv.className = "element-container";
-	newdiv.innerHTML = '<input type="text" placeholder="Enter your checkbox title here." required="required" name="data[' + addincount + '][0]" class="input checkbox-title" /><input type="hidden" required="required" name="data[' + addincount + '][1]" value="checkbox-title" /><input type="hidden" required="required" name="data[' + addincount + '][2]" value="' + elementName + '" /><button id="element-settings' + addincount + '" type="button" onclick="add_checkbox(checkbox' + addincount + ')" class="form-button add-checkbox-button"><i class="fa fa-plus"> checkbox</i></button><div id="checkbox' + addincount + '"></div>';
-
+	newdiv.innerHTML = '<input type="text" placeholder="Enter your checkbox title here." required="required" name="data[' + addincount + '][0]" class="input checkbox-element checkbox-title" /><input type="hidden" required="required" name="data[' + addincount + '][1]" value="checkbox-title" /><input type="hidden" required="required" name="data[' + addincount + '][2]" value="' + elementName + '" /><button id="element-settings' + addincount + '" type="button" onclick="add_checkbox(' + checkquot + ')" class="form-button add-checkbox-button"><i class="fa fa-plus"> checkbox</i></button><div id="checkbox' + addincount + '" class="checkbox-container"></div>';
 	document.getElementById(elementName).appendChild(newdiv);
 	addincount++;
 }
 
 function add_checkbox(elementName){
+	var linecount = 0;
 	var newdiv = document.createElement('div');
-	newdiv.innerHTML = '<div class="form-button checkbox-icon"><i class="fa fa-circle-o"></i></div><input type="text" placeholder="Enter your checkbox title here." required="required" name="data[' + addincount + '][3]" class="input checkbox-title" /><button id="element-settings' + addincount + '" type="button" onclick="#" class="form-button minus-button"><i class="fa fa-minus"></i></button>';
+	newdiv.className = "checkbox-line";
+	newdiv.innerHTML = '<div class="form-button checkbox-icon"><i class="fa fa-circle-o"></i></div><input type="text" placeholder="Label content." required="required" name="data[' + addincount + '][3][' + linecount + ']" class="input checkbox-element checkbox-content" /><input type="text" placeholder="Checkbox value" required="required" name="data[' + addincount + '][3][' + linecount + ']" class="input checkbox-element checkbox-content" /><button id="element-settings' + addincount + '" type="button" onclick="#" class="form-button minus-button"><i class="fa fa-minus"></i></button>';
 	document.getElementById(elementName).appendChild(newdiv);
+	linecount++;
 }
 
 function del_element(elementName){
 
-       var inputval = elementIndex;
-       var elementline = elementName + 'line' + elementIndex;
-       var elementbtn = elementName + 'btn' + elementIndex;
+	var inputval = elementIndex;
+	var elementline = elementName + 'line' + elementIndex;
+	var elementbtn = elementName + 'btn' + elementIndex;
 
-       document.getElementById(elementline).setAttribute("name", "delete");
-       document.getElementById(elementline).classList.add("line-delete");
-       document.getElementById(elementline).readOnly=true;
-       document.getElementById(elementbtn).innerHTML="+";
-       document.getElementById(elementbtn).classList.add("btn-delete");
-       document.getElementById(elementbtn).setAttribute("onClick", "enableInput('form', " + elementIndex + ");");
+	document.getElementById(elementline).setAttribute("name", "delete");
+	document.getElementById(elementline).classList.add("line-delete");
+	document.getElementById(elementline).readOnly=true;
+	document.getElementById(elementbtn).innerHTML="+";
+	document.getElementById(elementbtn).classList.add("btn-delete");
+	document.getElementById(elementbtn).setAttribute("onClick", "enableInput('form', " + elementIndex + ");");
 }
