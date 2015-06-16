70383eca7daee11112301b6338414f48ae0b211e
Keaton O <psychat@debian>
2015-06-16 10:37:53 +1000

    butter

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ var stormpath = require('express-stormpath');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 var pg = require('pg');
 
 // ROUTES
@@ -31,13 +29,10 @@ app.use(stormpath.init(app, {
   application:  process.env.STORMPATH_URL,
 }));
 
-// uncomment after placing your favicon in /public
+// **uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
@@ -51,46 +46,6 @@ app.use(function(req, res, next) {
 	var err = new Error('Not Found');
 	err.status = 404;
 	next(err);
-});
-
-// APPLICATION SERVER LISTENERS / SETUP
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-	app.use(function(err, req, res, next) {
-		res.status(err.status || 500);
-		res.render('error', {
-			message: err.message,
-			error: err
-		});
-	});
-}
-
-// set database connection
-
-app.get('/db', function (request, response) {
-	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-		client.query('SELECT * FROM test_table', function(err, result) {
-			done();
-			if (err)
-			 { console.error(err); response.send("Error " + err); }
-			else
-			 { response.send(result.rows); }
-		});
-	});
-})
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-	res.status(err.status || 500);
-	res.render('error', {
-		message: err.message,
-		error: {}
-	});
 });
 
 module.exports = app;
